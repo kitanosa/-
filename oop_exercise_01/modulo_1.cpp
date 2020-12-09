d1f0d2bb095aa69f8ae179a7260a0faf737dd1e9
kitanosa <43183258+kitanosa@users.noreply.github.com>
2020-12-10 00:56:39 +0300

    Update modulo_1.cpp

--- oop_exercise_01/modulo_1.cpp
+++ oop_exercise_01/modulo_1.cpp
@@ -5,23 +5,59 @@ using namespace std;
 class Modulo // программисты предпочитают называть класс с большой буквы
 {
 private:
-	int result;
+		int result;
 
-public:
-	int number, module_N;
+	public:
+		int number, module_N;
 
-	Modulo(int a, int N) {
-		number = a;
-		module_N = N;
-	}
+		Modulo(int a, int N) {
+			number = a;
+			module_N = N;
+		}
 
-	void sum(int first_number, int second_number, int module_N) {
-		result = (first_number + second_number) % module_N;
-	}
-	int get_sum() {
-		return result;
-	}
+		void sum(Modulo m1, Modulo m2) {
+			result = (m1.number + m2.number) % m1.module_N;
+		}
+
+		int get_sum() {
+			return result;
+		}
+
+		void diff(Modulo m1, Modulo m2) {// разность
+			result = (m1.number - m2.number) % m1.module_N;
+		}
+		int get_diff() {
+			return result;
+		}
+
+		void mult(Modulo m1, Modulo m2) {// произведение
+			result = (m1.number * m2.number) % m1.module_N;
+		}
+		int get_mult() {
+			return result;
+		}
+
+		void division(Modulo m1, Modulo m2) {// деление
+			result = (m1.number / m2.number) % m1.module_N;
+		}
+		int get_division() {
+			return result;
+		}
+
+		void comparison(Modulo m1, Modulo m2) {// сравнение
+			if ((m1.number % m1.module_N) > (m2.number % m1.module_N)) {
+				cout << "Остаток первого больше чем второго" << "\n";
+			}
+			else if ((m1.number % m1.module_N) < (m2.number % m1.module_N)) {
+				cout << "Остаток первого меньше чем второго" << "\n";
+			}
+			else {
+				cout << "Числа сравнимы" << "\n";
+			}
+		}
+
 };
+
 
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -36,8 +72,22 @@ int main() {
 
 	Modulo Example_1(a, N);
 	Modulo Example_2(b, N);
-	Example.sum(N);
+	Example_1.sum(Example_1, Example_2);
 	cout << "Первое число = " << Example_1.number << " Второе число = " << Example_2.number << " Модуль = " << N << "\n";
-	cout << "Сумма = " << Example.get_sum() << "\n";
+	cout << "Сумма = " << Example_1.get_sum() << "\n";
+
+	Example_1.diff(Example_1, Example_2);
+	cout << "Разность = " << Example_1.get_diff() << "\n";
+
+	Example_1.mult(Example_1, Example_2);
+	cout << "Произведение = " << Example_1.get_mult() << "\n";
+	if ((Example_1.number % N) == 0) {
+		cout << "Нельзя сделать деление" << "\n";
+	}
+	else {
+		Example_1.division(Example_1, Example_2);
+		cout << "Деление = " << Example_1.get_division() << "\n";
+	}
+	Example_1.comparison(Example_1, Example_2);
 	return 0;
 }
